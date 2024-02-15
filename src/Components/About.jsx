0f1bcd093de7080a1f1933cb9a083faa7e595f1c
Rifat-Shuvo <rhshuvo2812@gmail.com>
2024-02-15 11:41:27 +0600

    The revision is done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <div className="hero min-h-[70vh]" style={{ backgroundImage: 'url("/Banner.jpg")' }}>
+            <div className="hero min-h-[70vh] bg-contain" style={{ backgroundImage: 'url("/Blackbelt.png")' }}>
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
